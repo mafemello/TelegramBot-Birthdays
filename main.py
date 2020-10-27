f84b633cd7d9670dd8f407462bcfb7f4aa66a96a
PauloMRocha <paulo.mrocha@usp.br>
2020-10-27 18:07:16 -0300

    Some bugs corrected in daily function

--- main.py
+++ main.py
@@ -6,12 +6,12 @@
 from bot import Bot
 
 # Token provided by telegram
-TOKEN = 'xxxxxxxxxx'
+TOKEN = ''
 
 def main():
     bot = Bot(TOKEN) # creates bot
     bot.add_commands() # add commands
-    bot.turn_on() 
+    bot.turn_on()
     bot.turn_off()
 
 if __name__ == '__main__':
